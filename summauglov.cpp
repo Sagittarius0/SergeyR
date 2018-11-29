4e1d12092910a8b45c4e53fc5971ac390d776f17
Sagittarius0 <45123799+Sagittarius0@users.noreply.github.com>
2018-11-29 16:43:31 +0300

    Add files via upload

---
+++ summauglov.cpp
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/5a03b3f6a1c9040084001765
+//Find the total sum of angles in an n sided shape. N will be greater than 2.
+
+int angle(int n) {
+  return (n-2)*180;
+}
+int main(){
+	cout << angel(4) << endl;
+	return 0;
+}
